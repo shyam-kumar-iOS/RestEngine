e6d6c34cb0b64db750cacc4f9b0173f01aec262f
Shyam Kumar <shyamkumar@vajro.com>
2022-01-19 12:39:39 +0530

    initial commit

--- Sources/RestEngine/RestEngine.swift
+++ Sources/RestEngine/RestEngine.swift
@@ -1,3 +1,84 @@
-struct RestEngine {
-    var text = "Hello, World!"
+import Foundation
+
+
+public enum RequestType: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+}
+
+public class Resource<T: Codable> {
+    
+    var urlString: String
+    var parameters: [String : AnyObject]
+    var headers: [String : String]
+   public init(urlString: String, parameters: [String : AnyObject], headers: [String : String]) {
+            self.urlString = urlString
+            self.parameters = parameters
+            self.headers = headers
+        }
+}
+
+public class RestSession {
+    public init() {}
+    
+    public func makeGetRequest<T : Codable>(_ resource: Resource<T>,
+                                     completion: @escaping (T?) -> ()) {
+        makeRequest(.get,
+                    resource,
+                    completion: completion)
+    }
+    
+    public func makePostRequest<T : Codable>(_ resource: Resource<T>,
+                                      completion: @escaping (T?) -> ()) {
+        makeRequest(.post,
+                    resource,
+                    completion: completion)
+    }
+    
+    public func makePutRequest<T : Codable>(_ resource: Resource<T>,
+                                     completion: @escaping (T?) -> ()) {
+        makeRequest(.put,
+                    resource,
+                    completion: completion)
+    }
+    
+}
+
+extension RestSession {
+    
+    public func makeRequest<T : Codable>(_ type: RequestType,
+                                  _ resource: Resource<T>,
+                                  completion: @escaping (T?) -> ()) {
+        guard let serviceUrl = URL(string: resource.urlString) else { return }
+        
+        var request = URLRequest(url: serviceUrl)
+        request.httpMethod = type.rawValue
+        resource.headers.forEach { request.setValue($0.1, forHTTPHeaderField: $0.0) }
+        if type == .post || type == .put { request.httpBody = getHttpBody(from: resource.parameters) }
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            do {
+                if let data = data {
+                    let result = try JSONDecoder().decode(T.self, from: data)
+                    completion(result)
+                }
+            } catch let error {
+                if let decodingError = error as? DecodingError {
+                    print("Decoding error", decodingError, response ?? URLResponse())
+                }
+                completion(nil)
+            }
+        }.resume()
+    }
+    
+    public func getHttpBody(from params: [String: AnyObject]) -> Data? {
+        if let httpBody = try? JSONSerialization
+            .data(withJSONObject: params,
+                  options: []) {
+            return httpBody
+        }
+        return nil
+    }
+    
 }
--- Tests/RestEngineTests/RestEngineTests.swift
+++ Tests/RestEngineTests/RestEngineTests.swift
@@ -6,6 +6,6 @@
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
             // results.
-            XCTAssertEqual(RestEngine().text, "Hello, World!")
+         //   XCTAssertEqual(RestEngine().text, "Hello, World!")
         }
     }
